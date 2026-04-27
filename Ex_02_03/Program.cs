28a469dcc0c46ea4927bbe45759ef88cd7404008
RinaShimazu <2026opnewemp007@fullness.email>
2026-04-27 16:54:37 +0900

    演習3終了

--- Ex_02_03/Program.cs
+++ Ex_02_03/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Ex_02_03;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Ex_02_03;
 
 static class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("");
+    {   
+        int a = 10;
+        int b = 5;
+        
+        Console.WriteLine($"数値を入力してください->{a}");
+        Console.WriteLine($"{b}を加算した結果は{a + b}です");
+        Console.WriteLine($"{b}を減算した結果は{a - b}です");
     }
 }
