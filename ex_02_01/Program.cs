bba25be209045b3378f13ca6fadf705cb8cb2992
RinaShimazu <2026opnewemp007@fullness.email>
2026-04-27 16:48:56 +0900

    演習3開始

--- ex_02_01/Program.cs
+++ ex_02_01/Program.cs
@@ -1,11 +1,22 @@
 ﻿namespace ex_02_01;
 
-namespace Ex_02_01;
-
 static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool a = true, b = false;
+        Console.WriteLine($"a && a = {a && a}");
+        Console.WriteLine($"a && b = {a && b}");
+        Console.WriteLine($"b && a = {b && a}");
+        Console.WriteLine($"b && b = {b && b}");
+        Console.WriteLine($"a || a = {a || a}");
+        Console.WriteLine($"a || b = {a || b}");
+        Console.WriteLine($"b || a = {b || a}");
+        Console.WriteLine($"b || b = {b || b}");
+        int x = 1;
+        Console.WriteLine($"b && x++ > 1 = {b && x++ > 1}");
+        Console.WriteLine($"x = {x}");
+        Console.WriteLine($"b & x++ > 1 = {b & x++ > 1}");
+        Console.WriteLine($"x = {x}");  
     }
 }
