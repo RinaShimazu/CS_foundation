cff29769d21105bd062edf6f088e223256dedc5d
RinaShimazu <2026opnewemp007@fullness.email>
2026-04-28 11:36:09 +0900

    演習7終了

---
+++ Ex_02_07/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex_02_07;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(System.Console.ReadLine());
+        Console.WriteLine("数値を入力してください->" + number);
+
+        if (number > 0)
+        {
+            Console.WriteLine("正の数です");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("負の数です");
+        }
+        else
+        {
+            Console.WriteLine("0です");
+        }
+    }
+}
+
